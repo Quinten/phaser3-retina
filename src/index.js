ef143f0e92a008d4da0b9bb52ce2d78e1a04d180
Quinten Clause <quinten.clause@studioemma.eu>
2019-02-07 18:09:20 +0100

    Remove iframe focus thing
    as it is no longer needed we found out already :)

--- src/index.js
+++ src/index.js
@@ -6,25 +6,17 @@ import Level from './scenes/Level.js';
 var config = {
     type: Phaser.WEBGL,
     parent: 'phaser-game',
-    width: window.innerWidth * window.devicePixelRatio, // set game width by multiplying window width with devicePixelRatio
-    height: window.innerHeight * window.devicePixelRatio, // set game height by multiplying window height with devicePixelRatio
     backgroundColor: '#969fa3',
     pixelArt: true,
     zoom: 1 / window.devicePixelRatio, // Set the zoom to the inverse of the devicePixelRatio
+    width: window.innerWidth * window.devicePixelRatio, // set game width by multiplying window width with devicePixelRatio
+    height: window.innerHeight * window.devicePixelRatio, // set game height by multiplying window height with devicePixelRatio
     scene: [
         Boot,
         Preloader,
         Level
     ]
 };
-
-// improve iframe focus
-window.addEventListener('load', function () {
-    window.focus();
-    document.body.addEventListener('click',function(e) {
-        window.focus();
-    },false);
-});
 
 // start game
 window.game = new Phaser.Game(config);
