e04098a024391c83ac37487b1d681d8c6899e7f9
Quinten Clause <quinten.clause@studioemma.eu>
2019-02-09 19:16:20 +0100

    Adjust code for the Phaser 3.16 scale manager

--- src/index.js
+++ src/index.js
@@ -5,12 +5,14 @@ import Level from './scenes/Level.js';
 
 var config = {
     type: Phaser.WEBGL,
-    parent: 'phaser-game',
     backgroundColor: '#969fa3',
     pixelArt: true,
-    zoom: 1 / window.devicePixelRatio, // Set the zoom to the inverse of the devicePixelRatio
-    width: window.innerWidth * window.devicePixelRatio, // set game width by multiplying window width with devicePixelRatio
-    height: window.innerHeight * window.devicePixelRatio, // set game height by multiplying window height with devicePixelRatio
+    scale: {
+        mode: Phaser.Scale.NONE, // we will resize the game with our own code (see Boot.js)
+        width: window.innerWidth * window.devicePixelRatio, // set game width by multiplying window width with devicePixelRatio
+        height: window.innerHeight * window.devicePixelRatio, // set game height by multiplying window height with devicePixelRatio
+        zoom: 1 / window.devicePixelRatio // Set the zoom to the inverse of the devicePixelRatio
+    },
     scene: [
         Boot,
         Preloader,
